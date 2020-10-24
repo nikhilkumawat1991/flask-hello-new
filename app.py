5b24ea4f8315fc3dbefa42530f9d94c6a31e97b5
nikhil-kumawat-1991 <nikhil.kumawat@matellio.com>
2020-10-24 15:18:13 +0100

    changing app file

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route("/")
 def index():
     return "<h1>Hello Azure!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9060)
+
