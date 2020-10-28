291bfa047bb5c03e1a53d7833421e2b8ec5d5e1e
nikhil kumawat <nikhil.kumawat@matellio.com>
2020-10-28 23:08:05 +0530

    adding hackathon initial UI

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+"""
+Veea CONFIDENTIAL © 2020
+All rights reserved NOTICE: All information contained herein is, and remains
+the property of Veea and its suppliers, if any. The intellectual and technical
+concepts contained herein are proprietary to Veea, and its suppliers  and may
+be covered by U.S. and Foreign Patents, patents in process, and are protected by
+trade secret or copyright law. Dissemination of this information or reproduction
+of this material is strictly forbidden unless prior written permission is obtained
+from Veea.
+"""
+###########################################################################
+#LORAWAN CONFIGURATION MANAGER
+###########################################################################
+
+import logging
+import json
+import traceback
 from flask import Flask, request, render_template,jsonify, redirect, url_for
+from flask_cors import CORS, cross_origin
+import os
+import yaml
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
-@app.route("/")
-def hello():
+@app.route('/')
+def home():
     return render_template('index.html')
+
+
+@app.route('/services', methods=['GET','POST'])
+@cross_origin(supports_credentials=True)
+def services():
+    logging.debug("Redirecting to Device Connect Page")
+    return render_template("services.html")    
 
 
 if __name__ == '__main__':
