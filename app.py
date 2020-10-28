137c1ede24b2c0a0442bdc2aa02891e29dbc8c74
nikhil kumawat <nikhil.kumawat@matellio.com>
2020-10-28 23:16:33 +0530

    changing app file

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
-"""
-Veea CONFIDENTIAL © 2020
-All rights reserved NOTICE: All information contained herein is, and remains
-the property of Veea and its suppliers, if any. The intellectual and technical
-concepts contained herein are proprietary to Veea, and its suppliers  and may
-be covered by U.S. and Foreign Patents, patents in process, and are protected by
-trade secret or copyright law. Dissemination of this information or reproduction
-of this material is strictly forbidden unless prior written permission is obtained
-from Veea.
-"""
-###########################################################################
-#LORAWAN CONFIGURATION MANAGER
-###########################################################################
-
 import logging
 import json
 import traceback
 from flask import Flask, request, render_template,jsonify, redirect, url_for
-from flask_cors import CORS, cross_origin
 import os
 import yaml
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
 
 
 @app.route('/')
@@ -30,7 +14,6 @@ def home():
 
 
 @app.route('/services', methods=['GET','POST'])
-@cross_origin(supports_credentials=True)
 def services():
     logging.debug("Redirecting to Device Connect Page")
     return render_template("services.html")    
